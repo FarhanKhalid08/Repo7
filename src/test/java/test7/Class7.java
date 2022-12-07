e2a0ad1bd12f4e30d9feeed82f5ba1c0e373493e
farhankhalid08 <farhan.khalid@hotmail.co.uk>
2022-12-06 23:11:15 -0500

    new

--- src/test/java/test7/Class7.java
+++ src/test/java/test7/Class7.java
@@ -2,17 +2,16 @@ package test7;
 
 import java.time.Duration;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.asserts.SoftAssert;
 
 public class Class7 {
 public static WebDriver driver;
-public static SoftAssert softassert = new SoftAssert();
 public static WebDriverWait wait;
 
 public static void main (String[] args) {
@@ -21,7 +20,7 @@ public static void main (String[] args) {
 	driver.get("http://rediff.com");
 	
 	WebElement signinlink= driver.findElement(By.cssSelector("a.signin"));
-	softassert.assertTrue(signinlink.isDisplayed());
+	Assert.assertTrue(signinlink.isDisplayed());
 	wait = new WebDriverWait(driver,Duration.ofSeconds(10));
 	wait.until(ExpectedConditions.elementToBeClickable(signinlink));
 	signinlink.click();
@@ -29,16 +28,15 @@ public static void main (String[] args) {
 	WebElement username= driver.findElement(By.id("login1"));
 	wait = new WebDriverWait (driver,Duration.ofSeconds(5));
 	wait.until(ExpectedConditions.elementToBeClickable(username));
-	softassert.assertTrue(username.isDisplayed());
+	Assert.assertTrue(username.isDisplayed());
 	username.sendKeys("Farhankhalid920@hotmail.com");
 	
 	WebElement password= driver.findElement(By.id("password"));
 	wait = new WebDriverWait(driver,Duration.ofSeconds(5));
 	wait.until(ExpectedConditions.elementToBeClickable(password));
-	softassert.assertTrue(password.isDisplayed());
+	Assert.assertTrue(password.isDisplayed());
 	password.sendKeys("subhan");
 	
-	softassert.assertAll();
 	driver.quit();
      
 }
